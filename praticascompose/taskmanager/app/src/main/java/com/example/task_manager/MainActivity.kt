0e623d1eca0d38fb28d6e1879045f87165e33662
Sofia Leal <sofialealoliveira@gmail.com>
2025-01-26 23:01:57 -0300

    Adiciona pastas dos apps TaskManager e Quadrantes

--- praticascompose/taskmanager/app/src/main/java/com/example/task_manager/MainActivity.kt
+++ praticascompose/taskmanager/app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.learn_together
+package com.example.task_manager
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,15 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.learn_together.ui.theme.LearntogetherTheme
+import com.example.task_manager.ui.theme.TaskmanagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearntogetherTheme {
-
+            TaskmanagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
@@ -36,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LearntogetherTheme {
+    TaskmanagerTheme {
         Greeting("Android")
     }
 }
